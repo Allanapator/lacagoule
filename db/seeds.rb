31ccc4c9141f353da154d1e370e76bb9dc23c4aa
Allan ROUET-ALVARO <allan.rouet@gmail.com>
2020-06-15 17:57:14 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Hood.new(name: "Bob", city: "Bordeaux", description: "J'aime niquer des mères", skills: "ceinture noir", price: 800)
+Hood.new(name: "Alex", city: "Bordeaux", description: "J'aime niquer des mères", skills: "ceinture noir", price: 800)
+Hood.new(name: "Allan", city: "Bordeaux", description: "J'aime niquer des mères", skills: "ceinture noir", price: 800)
+Hood.new(name: "Julie", city: "Bordeaux", description: "J'aime niquer des mères", skills: "ceinture noir", price: 800)
+Hood.new(name: "Jules", city: "Bordeaux", description: "J'aime niquer des mères", skills: "ceinture noir", price: 800)
+Hood.new(name: "Matthieu", city: "Bordeaux", description: "J'aime niquer des mères", skills: "ceinture noir", price: 800)
