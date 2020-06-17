a4073732145bb4696b9dee825dcb84a743af231a
Alexis Flipo <alexis.flipo1@gmail.com>
2020-06-17 19:49:50 +0200

    search bar

--- app/controllers/hoods_controller.rb
+++ app/controllers/hoods_controller.rb
@@ -1,14 +1,20 @@
 class HoodsController < ApplicationController
   layout "applicationn", only: [:show]
   def index
-     if params[:query].present?
-      @hoods = Hood.where(city: params[:query])
+     if params[:q].present?
+        @hoods = Hood.where("city LIKE ?", "%" + params[:q] + "%")
     else
-      render 'index'
+        @hoods = Hood.all
     end
   end
 
   def show
     @hood = Hood.find(params[:id])
   end
+
+  def hoods_params
+    params.require(:hood).permit(:query)
+  end
+
+
 end
