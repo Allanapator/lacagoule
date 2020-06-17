f48e0caf2a93ee484b8349d1f17ddc8a7ad73e1e
Alexis Flipo <alexis.flipo1@gmail.com>
2020-06-17 15:31:26 +0200

    middle presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,32 +8,20 @@
 Contract.destroy_all
 Hood.destroy_all
 User.delete_all
-
 puts "User"
-
 User.create!(name: "Kebab", city: "Bordeaux", email: "kebabhmstr@gmail.com", password:"secret")
-User.create!(name: "Kebab", city: "Bordeaux", email: "allan@gmail.com", password:"secret")
-
+User.create!(name: "Allan", city: "Bordeaux", email: "allan@gmail.com", password:"secret")
 # User.delete(name: "LilDicky", city: "Bordeaux", email: "lildickyhmstr@gmail.com")
 # User.delete(name: "Kebab", city: "Bordeaux", email: "kebabhmstr@gmail.com")
 # User.delete(name: "Mikado", city: "Mulhouse", email: "mikadohmstr@gmail.com")
-
-
 puts "HOOD"
-
 Hood.create!(name: "Bob", city: "Bordeaux", description: "Chaud en arts martiaux", skills: "ceinture noire Judo", price: 800, rating: 3, violence_range: 2)
-Hood.create!(name: "Alex", city: "Bordeaux", description: "Chaud en arts martiaux", skills: "ceinture noire Karate", price: 700, rating: 5, violence_range: 3)
-Hood.create!(name: "Allan", city: "Bordeaux", description: "Chaud en arts martiaux", skills: "Champion de boxe", price: 400, rating: 3, violence_range: 2)
-Hood.create!(name: "Julie", city: "Bordeaux", description: "Chaud en arts martiaux", skills: "Ancien taulard", price: 200, rating: 4, violence_range: 1)
-Hood.create!(name: "Jules", city: "Bordeaux", description: "Chaud en arts martiaux", skills: "Bucheron", price: 500, rating: 4, violence_range: 3)
-Hood.create!(name: "Matthieu", city: "Bordeaux", description: "Chaud en arts martiaux", skills: "Slap world champion", price: 1000, rating: 3, violence_range: 2)
-Hood.create!(name: "Fabiola", city: "Bordeaux", description: "Pro des coups de parapluie", skills: "Jiu-Jitsu", price: 300, rating: 3, violence_range: 3)
-Hood.create!(name: "Carlito", city: "Bordeaux", description: "Manie sa hâche parfaitement", skills: "Fils de Ragnar Lodbrok", price: 800, rating: 3, violence_range: 3)
-
-
-
+Hood.create!(name: "Alex", city: "Bordeaux", description: "J'adore la castagne", skills: "ceinture noire Karate", price: 700, rating: 5, violence_range: 3)
+Hood.create!(name: "Allan", city: "Bordeaux", description: "J'aime le risque", skills: "Champion de boxe", price: 400, rating: 3, violence_range: 2)
+Hood.create!(name: "Julie", city: "Bordeaux", description: "Hooligan le W-E", skills: "Ancien taulard", price: 200, rating: 4, violence_range: 1)
+Hood.create!(name: "Jules", city: "Bordeaux", description: "Combattant de rue", skills: "Bucheron", price: 500, rating: 4, violence_range: 3)
+Hood.create!(name: "Matthieu", city: "Bordeaux", description: "Besoin de thune", skills: "Slap world champion", price: 1000, rating: 3, violence_range: 2)
+Hood.create!(name: "Fabiola", city: "Bordeaux", description: "Pro du parapluie", skills: "Jiu-Jitsu", price: 300, rating: 3, violence_range: 3)
+Hood.create!(name: "Carlito", city: "Bordeaux", description: "Attention je mords", skills: "Fils de Ragnar Lodbrok", price: 800, rating: 3, violence_range: 3)
 puts "Contract"
-Contract.create!(name: "Casser la gueule de ma voisine", description: "Ma voisine me fait chier je veux que qqn aille lui casser la gueule mais pas trop non plus", location: "Bordeaux", violence_required: 1, user_id: User.first.id, hood_id: Hood.first.id)
-Contract.create!(name: "j'aime pas ce mec", description: "Bjr merci de KC au max la geul de ce mec que jem pas merci il a réyé ma kess", location: "Bordeaux", violence_required: 2, user_id: User.first.id, hood_id: Hood.first.id)
-Contract.create!(name: "Mon boss m'a pas payé", description: "Mon boss a encore oublié de me payer ce mois ci c'est la goutte d'eau", location: "Bordeaux", violence_required: 3, user_id: User.first.id, hood_id: Hood.first.id)
 puts "finished"
