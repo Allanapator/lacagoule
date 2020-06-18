487950e21f7bced79da757e588ef5c49d6c4e63c
Alexis Flipo <alexis.flipo1@gmail.com>
2020-06-18 22:37:16 +0200

    filter

--- app/controllers/hoods_controller.rb
+++ app/controllers/hoods_controller.rb
@@ -1,12 +1,13 @@
 class HoodsController < ApplicationController
   layout "applicationn", only: [:show]
- def index
-  if Hood.search(params[:search]).blank?
-    @hoods = Hood.search(params[:search])
-  else
-    @hoods = Hood.search(params[:search])
-  end
- end
+   def index
+    if Hood.search(params[:search]).blank?
+      @hoods = Hood.search(params[:search])
+    else
+      @hoods = Hood.search(params[:search])
+    end
+   end
+
   def show
     @hood = Hood.find(params[:id])
   end
